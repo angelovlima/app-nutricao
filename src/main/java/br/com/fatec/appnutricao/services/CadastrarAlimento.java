ea4ea5691496f7378c658171a8399f2551a3f89a
tabathafroes <tabathafroes@gmail.com>
2020-11-18 21:08:38 -0300

    Ajuste cadastrar alimento

--- src/main/java/br/com/fatec/appnutricao/services/CadastrarAlimento.java
+++ src/main/java/br/com/fatec/appnutricao/services/CadastrarAlimento.java
@@ -1,11 +1,45 @@
 package br.com.fatec.appnutricao.services;
 
+import java.util.List;
+import java.util.Scanner;
 import br.com.fatec.appnutricao.model.alimento.Alimento;
+import br.com.fatec.appnutricao.model.alimento.InformacaoNutricional;
+import br.com.fatec.appnutricao.model.alimento.TabelaNutricional;
 
 public class CadastrarAlimento {
-
-	public Alimento cadastrarAlimento() {
-		return null;
+	
+	@SuppressWarnings("resource")
+	public void cadastrarAlimento(List<Alimento> alimentosCadastrados, TabelaNutricional tabelaNutricional) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o grupo do alimento que deseja cadastrar:");
+		System.out.println("Ex: 1 - Laticínios|2 - Carne|3 - Fruta e Verdura|4 - Grãos|5 - Gordura(vegetal ou animal)");
+		String tipo = sc.nextLine();
+		System.out.println("Digite o nome do alimento:");
+		String nome = sc.nextLine();
+		for(Alimento alimento: alimentosCadastrados) {
+			if(nome.equalsIgnoreCase(alimento.getNome())) {
+				System.out.println("Alimento já cadastrado. Tente novamente!");
+				break;
+			}
+		}
+		System.out.println("Cadastre os dados da tabela nutricional do alimento:");
+		for(InformacaoNutricional item: tabelaNutricional.getInformacoesNutricionais()) {
+			System.out.println(item.getNome());
+			System.out.println("Digite a quantidade:");
+			double quantidade = sc.nextDouble();
+			item.setQuantidade(quantidade);
+			System.out.println("Digite o valor diário:");
+			double valorDiario = sc.nextDouble();
+			item.setValorDiario(valorDiario);
+			
+			
+			
+		}
+		
+		Alimento alimentoUnidade = new Alimento(tipo, nome, tabelaNutricional);
+		alimentosCadastrados.add(alimentoUnidade);
+		
+		
 		
 	}
 }
