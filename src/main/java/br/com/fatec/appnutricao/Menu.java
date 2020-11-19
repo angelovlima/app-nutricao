b34dde45a94e4bb9d801775287eff273415ba9c5
tabathafroes <tabathafroes@gmail.com>
2020-11-18 21:35:43 -0300

    ajuste Menu

--- src/main/java/br/com/fatec/appnutricao/Menu.java
+++ src/main/java/br/com/fatec/appnutricao/Menu.java
@@ -7,6 +7,9 @@ import br.com.fatec.appnutricao.model.alimento.Alimento;
 import br.com.fatec.appnutricao.model.alimento.InformacaoNutricional;
 import br.com.fatec.appnutricao.model.alimento.TabelaNutricional;
 import br.com.fatec.appnutricao.services.BaseDados;
+import br.com.fatec.appnutricao.services.CadastrarAlimento;
+import br.com.fatec.appnutricao.services.CadastrarCombinacao;
+import br.com.fatec.appnutricao.services.CadastrarReceita;
 
 public class Menu {
 	
@@ -15,6 +18,9 @@ public class Menu {
 		BaseDados baseDados = new BaseDados();
 		TabelaNutricional tabelaNutricional = baseDados.popularTabelaNutricional();
 		List<Alimento> alimentos = baseDados.popularListaAlimentos();
+		CadastrarAlimento cadastrarAlimento = new CadastrarAlimento();
+		CadastrarCombinacao cadastrarCombinacao = new CadastrarCombinacao();
+		CadastrarReceita cadastrarReceita = new CadastrarReceita();
 
 //		for (Alimento alimento : alimentos) {
 //			TabelaNutricional tabelaNutricionalAlimento = alimento.getTabelaNutricional();
@@ -35,16 +41,27 @@ public class Menu {
 		while(menu != 0) {
 			
 			System.out.println("\n+++++++++++++++++++++++MENU+++++++++++++++++++++++ \n" + "0: Sair \n"
-					+ "1: Cadastrar Bovino \n" + "2: Cadastrar Suino \n" + "3: Checar preço de algum Animal \n"
-					+ "4: Checar a quantidade de Animais Cadastrados \n" + "5: Checar preço total do Rebanho \n"
-					+ "6: Checar o peso total do Rebanho \n" + "7: Checar animais por Gênero \n"
-					+ "8: Checar a Porcentagem do rebanho Vacinado e quantos Faltam Vacinar \n"
-					+ "9: Vender Animal \n");
+					+ "1: Cadastrar Alimento \n" + "2: Cadastrar Combinação \n" + "3: Cadastrar Receita \n"
+					+ "4: Relatório de Alimentos \n" + "5: Relatório de Combinações \n"
+					+ "6: Relatório de Refeições \n");
 			
 			menu = sc.nextInt();
 			
 			switch(menu) {
 			case 1: 
+				cadastrarAlimento.cadastrarAlimento(alimentos, tabelaNutricional);
+			case 2:
+				cadastrarCombinacao.cadastrarCombinacao(alimentos);
+			case 3:
+				cadastrarReceita.cadastrarReceita(alimentos);
+			case 4:
+				
+			case 5:
+				
+			case 6:
+				
+			default:
+				break;
 			}
 		}
 	}
